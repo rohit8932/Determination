d7b221354e334182aa443fa7d96239c37e48fce0
rohit8932 <rohitrajak8932@gmail.com>
2019-11-16 23:47:02 +0530

    Graph using clrs

--- src/code/Graph_CLRS.java
+++ src/code/Graph_CLRS.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
 
 public class Graph_CLRS {
 	List<List<Vertex>> adjList = new ArrayList<>();
@@ -38,36 +39,65 @@ public class Graph_CLRS {
 		for(Edge e: edges) {
 			adjList.get(e.src.vertex).add(e.destn);
 		}
-		
+	}
+	
+	public void reset() {
 		for(int i = 0; i < adjList.size(); i++) {
-			for(int j = 0; j < adjList.get(i).size(); j++) {
-				Vertex v = adjList.get(i).get(j);
+			for(Vertex v : adjList.get(i)) {
 				v.color = Color.WHITE;
 				v.pred = null;
-				v.distance = 0;
+				v.distance = -1;
 			}
 		}
 	}
 	
+	//time- O(V+E) space- O(V)
 	public void bfs(Vertex s) {
+		reset(); //this is to ensure we are not re-using vertex which has been used.eg if you run bfs and dfs one after another it will give output correct
 		Queue<Vertex> q = new LinkedList<>();
 		q.add(s);
+		s.color = Color.WHITE;
+		s.distance = -1;
+		s.pred = null;
 		while(!q.isEmpty()) {
 			Vertex temp = q.remove();
 			System.out.print(temp.vertex + " ");
 			for(int i = 0; i < adjList.get(temp.vertex).size(); i++) {
 				Vertex nebr = adjList.get(temp.vertex).get(i);
-				if(nebr.color.equals(Color.WHITE)) {					
-					q.add(nebr);
+				if(nebr.color.equals(Color.WHITE)) {	
 					nebr.color = Color.GREY;
 					nebr.pred = temp;
 					nebr.distance = temp.distance + 1;
+					q.add(nebr);
 				}
 			}
-			//we have visited all neighbour of temp so we mark it as black
+			//we have visited all neighbours of temp so we mark it as black
 			temp.color = Color.BLACK;
 		}
 	}
 	
+	//time- O(V+E) space- O(V)
+	public void dfs(Vertex src) {
+		reset();
+		Stack<Vertex> s = new Stack<>();
+		s.push(src);
+		src.color = Color.GREY;
+		src.distance = 0;
+		src.pred = null;
+		while(!s.empty()) {
+			Vertex temp = s.pop();
+			System.out.print(temp.vertex + " ");
+			for(int i = 0; i < adjList.get(temp.vertex).size(); i++) {
+				Vertex v = adjList.get(temp.vertex).get(i);
+				if(v.color.equals(Color.WHITE)) {
+					v.color = Color.GREY;
+					v.pred = temp;
+					v.distance = temp.distance + 1;
+					s.add(v);
+				}
+			}
+			temp.color = Color.BLACK;
+		}
+	}
 
 }
--- src/code/Graph_CLRS_Main.java
+++ src/code/Graph_CLRS_Main.java
@@ -1,19 +1,43 @@
 package code;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import code.Graph_CLRS.Vertex;
+
 public class Graph_CLRS_Main {
 	public static void main(String[] args) {
+		/*
+		 * 	0----->1     2
+		 *  |     Y|    /|
+		 *  |    / |   / |
+		 *  |   /  |  /  |
+		 *  |  /   | /   |
+		 *	v /    VY    V
+		 *  3<-----4     5
+		 * 
+		 */
 		Graph_CLRS g = new Graph_CLRS();
-		List<Graph_CLRS.Edge> edges = Arrays.asList(new Graph_CLRS.Edge(new Graph_CLRS.Vertex(0), new Graph_CLRS.Vertex(1)),
-				new Graph_CLRS.Edge(new Graph_CLRS.Vertex(1), new Graph_CLRS.Vertex(3)),
-				new Graph_CLRS.Edge(new Graph_CLRS.Vertex(1), new Graph_CLRS.Vertex(2)),
-				new Graph_CLRS.Edge(new Graph_CLRS.Vertex(4), new Graph_CLRS.Vertex(3)),
-				new Graph_CLRS.Edge(new Graph_CLRS.Vertex(1), new Graph_CLRS.Vertex(5)),
-				new Graph_CLRS.Edge(new Graph_CLRS.Vertex(2), new Graph_CLRS.Vertex(4)));
+		List<Graph_CLRS.Vertex> vertices = new ArrayList<>();
+		vertices.add(new Graph_CLRS.Vertex(0));
+		vertices.add(new Graph_CLRS.Vertex(1));
+		vertices.add(new Graph_CLRS.Vertex(2));
+		vertices.add(new Graph_CLRS.Vertex(3));
+		vertices.add(new Graph_CLRS.Vertex(4));
+		vertices.add(new Graph_CLRS.Vertex(5));
+		
+		List<Graph_CLRS.Edge> edges = Arrays.asList(new Graph_CLRS.Edge(vertices.get(0), vertices.get(1)),
+				new Graph_CLRS.Edge(vertices.get(0), vertices.get(3)),
+				new Graph_CLRS.Edge(vertices.get(1), vertices.get(4)),
+				new Graph_CLRS.Edge(vertices.get(4), vertices.get(3)),
+				new Graph_CLRS.Edge(vertices.get(3), vertices.get(1)),
+				new Graph_CLRS.Edge(vertices.get(2), vertices.get(4)),
+				new Graph_CLRS.Edge(vertices.get(2), vertices.get(5)));
 		g.graph(edges);
-		g.bfs(new Graph_CLRS.Vertex(0));
+		g.bfs(vertices.get(0));
+		System.out.println();
+		g.dfs(vertices.get(0));
 	}
 
 }
